8078bc6eb5e04f94875eb4871355c9ce21b46991
KitaitiMakoto <KitaitiMakoto@gmail.com>
2014-01-24 20:33:38 +0900

    Remove unused cookbook

--- site-cookbooks/ci.phantomcss-ci.local/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'ci.phantomcss-ci.local'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures ci.phantomcss-ci.local'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
--- site-cookbooks/ci.phantomcss-ci.local/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: ci.phantomcss-ci.local
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-log "ci.phantomcss-ci.local"
