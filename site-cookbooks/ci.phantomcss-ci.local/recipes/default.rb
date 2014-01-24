60eba056971da0dff8c1e522f358464de9173cdc
KitaitiMakoto <KitaitiMakoto@gmail.com>
2014-01-24 20:06:29 +0900

    Install packages for PhantomCSS

---
+++ site-cookbooks/ci.phantomcss-ci.local/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: ci.phantomcss-ci.local
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "ci.phantomcss-ci.local"
---
+++ site-cookbooks/phantomcss/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: phantomcss
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+%w[
+  libcairo2-dev
+  libjpeg8-dev
+  libpango1.0-dev
+  libgif-dev
+].each do |pkg|
+  package pkg
+end
+
