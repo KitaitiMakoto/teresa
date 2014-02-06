f8fe1901dff2c8a7cf925b388195de6bb12081f1
KitaitiMakoto <KitaitiMakoto@gmail.com>
2014-02-06 17:47:22 +0900

    Create Rails project
    $ bundle exec rails new phantomcss-ci --skip-bundle --skip-active-record

---
+++ phantomcss-ci/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhantomcssCi::Application.config.secret_key_base = '99a169d6135919eba5ec33bedad8a53391e2b2583cb5fc48d4bd251a676a670e53939eeb256d43f1c1710eede09a31432f82eba373f3930107df2addd3a427f6'
---
+++ phantomcss-ci/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhantomcssCi::Application.config.session_store :cookie_store, key: '_phantomcss-ci_session'
